b5c3020e706c2e344aee1f5ed187d9b1ac72c2bf
knewnzm <plutom035@gmail.com>
2023-04-24 21:33:20 +0900

    addToList

--- shoppingApp/main.js
+++ shoppingApp/main.js
@@ -3,6 +3,8 @@
 const add = document.querySelector('.add-btn');
 const list = document.querySelector('.list__main');
 const inputAdd = document.querySelector('.list__footer');
+// const delete = document.querySelector('.del-btn');
+
 
 add.addEventListener( 'click', () => {
     console.log('시험');
@@ -10,5 +12,37 @@ add.addEventListener( 'click', () => {
     inputAdd.innerHTML = `
     <input class="input" placeholder="ex) 청바지" />
     <button class="enter">enter</button>
-    `
-})
+    `;
+
+    const enter = document.querySelector('.enter');
+    const input = document.querySelector('.input');
+
+    enter.addEventListener('click', () => {
+        const text = input.value.trim();
+    
+        if (text !== '') {
+        console.log(text);
+        addToList(text);
+        input.value = '';
+        input.focus();
+        
+    }
+    });
+});
+
+
+function addToList(text) {
+    const list = document.querySelector('#list');
+    const newListItem = document.createElement('li');
+    const del = document.createElement('button');
+
+    newListItem.classList.add('list__item');
+    newListItem.innerHTML = text;
+    list.appendChild(newListItem);
+
+    // del.classList.add('del-btn');
+    // del.innerHTML = '삭제';
+    // console.log(del);
+    // list.appendChild(newListItem);
+}
+
